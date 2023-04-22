d1419e8d7981ae1aabcd6cc992116858a55bd13f
Darya Suravtsova <darblnkapm@gmail.com>
2023-04-22 23:29:00 +0300

    feat: add task 5

--- dbTask5.sql
+++ dbTask5.sql
@@ -1,4 +1,3 @@
---task5
 --у кого списывает андреева анастасия 
 SELECT groupmate1.name
  , groupmate2.name AS [у кого списывает]
@@ -34,3 +33,31 @@ FROM groupmate AS groupmate1
  , school
 WHERE MATCH(groupmate1-(cheatsFrom)->groupmate2-(schoolLike)->school)
  AND groupmate1.name = N'Гринько Кирилл Владимирович';
+
+
+-- Список студентов, которые оценили школу, в которой они учились, на 9 или 10 баллов
+SELECT groupmate.name AS [Студент]
+      ,school.name AS [Школа]
+      ,schoolLike.rating AS [Оценка]
+FROM groupmate
+    ,schoolLike
+    ,school
+WHERE MATCH(groupmate-(schoolLike)->school)
+      AND rating >= 9;
+
+
+-- Список студентов, которые из одной области с Григорьевой Марией
+SELECT groupmate1.name AS [Первый студент]
+      ,groupmate2.name AS [Второй студент]
+	    ,school.hometown AS [Область первого студента]
+	    ,hometown.region AS [Область второго студента]
+FROM groupmate AS groupmate1
+    ,groupmate AS groupmate2
+    ,whereAreYouFrom
+    ,hometown
+    ,schoolLike
+    ,school
+WHERE MATCH(hometown<-(whereAreYouFrom)-groupmate1
+      AND groupmate2-(schoolLike)->school)
+	    AND school.hometown = hometown.region
+	    AND groupmate1.name = N'Григорьева Мария Георгиевна';
